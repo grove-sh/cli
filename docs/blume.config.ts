0c0c1211e289780cd3779159c24d613b60e4ce01
Chris Allen <chris@apaxsoftware.com>
2026-09-19 16:10:03 -0400

    Ran blume init and pnpm install

---
+++ docs/blume.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "blume";
+
+export default defineConfig({
+  title: "grove",
+  description: "Documentation powered by Blume.",
+  content: {
+    root: "content",
+  },
+});
